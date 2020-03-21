b1cc9493ed39cd99c94a771cb351bc3162c4c7ce
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-03-21 17:35:47 +0900

    takanawa gateway

---
+++ src/main/kotlin/dev/yoghurt1131/TakanawaGatway/TakanawaGatwayApplication.kt
@@ -0,0 +1,11 @@
+package dev.yoghurt1131.TakanawaGatway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TakanawaGatwayApplication
+
+fun main(args: Array<String>) {
+	runApplication<TakanawaGatwayApplication>(*args)
+}
---
+++ src/test/kotlin/dev/yoghurt1131/TakanawaGatway/TakanawaGatwayApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yoghurt1131.TakanawaGatway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TakanawaGatwayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
