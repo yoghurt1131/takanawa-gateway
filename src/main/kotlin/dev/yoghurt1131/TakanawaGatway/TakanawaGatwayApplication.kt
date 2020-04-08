a3dfb85c5076883a1980e35d4b168663a0e219e2
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-04-08 23:23:57 +0900

    introduce resilience4j circuit breaker

--- src/main/kotlin/dev/yoghurt1131/TakanawaGatway/TakanawaGatwayApplication.kt
+++ src/main/kotlin/dev/yoghurt1131/TakanawaGatway/TakanawaGatwayApplication.kt
@@ -2,9 +2,21 @@ package dev.yoghurt1131.TakanawaGatway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @SpringBootApplication
-class TakanawaGatwayApplication
+class TakanawaGatwayApplication {
+
+
+	@Bean
+	fun fallback() = router {
+		GET("/fallback", accept(MediaType.TEXT_PLAIN), { ServerResponse.ok().bodyValue("Fallback")})
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<TakanawaGatwayApplication>(*args)
