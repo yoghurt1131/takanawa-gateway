c3dbef7df152771148b713e4445c83f7bca9e47e
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-04-12 17:10:48 +0900

    customize resillence4j

--- src/main/kotlin/dev/yoghurt1131/TakanawaGatway/TakanawaGatwayApplication.kt
+++ src/main/kotlin/dev/yoghurt1131/TakanawaGatway/TakanawaGatwayApplication.kt
@@ -1,5 +1,7 @@
 package dev.yoghurt1131.TakanawaGatway
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -14,6 +16,16 @@ class TakanawaGatwayApplication {
 	@Bean
 	fun fallback() = router {
 		GET("/fallback", accept(MediaType.TEXT_PLAIN), { ServerResponse.ok().bodyValue("Fallback")})
+	}
+
+	@Bean
+	fun resilience4j(): CircuitBreaker {
+		val config = CircuitBreakerConfig.custom()
+				.slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+				.slidingWindowSize(10)
+				.minimumNumberOfCalls(3)
+				.build()
+		return CircuitBreaker.of("resilience4j", config)
 	}
 
 }
